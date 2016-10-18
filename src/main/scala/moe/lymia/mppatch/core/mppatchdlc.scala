abd15f451b2758fec6a8ef62fff3b06574d5a05b
Lymia Aluysia <lymia@lymiahugs.com>
2016-10-18 04:35:41 -0500

    Begin refactor or MPPatchDLC

--- src/main/scala/moe/lymia/mppatch/core/mppatchdlc.scala
+++ src/main/scala/moe/lymia/mppatch/core/mppatchdlc.scala
@@ -31,36 +31,35 @@ import moe.lymia.mppatch.util.{IOUtils, res}
 
 import scala.xml.XML
 
-case class MPPatchLuaOverride(injectBefore: Seq[String] = Seq(), injectAfter: Seq[String] = Seq())
-object MPPatchDLC {
-  val DLC_UUID          = UUID.fromString("df74f698-2343-11e6-89c4-8fef6d8f889e")
-  val DLC_VERSION       = 1
-  val DLC_UPDATEVERSION = 1
+trait MPPatchFileSource {
+  def loadHook   (name: String): String
+  def loadLibrary(name: String): String
+  def loadScreen (name: String): String
+  def loadText   (name: String): String
+}
+object ResourceSource extends MPPatchFileSource {
+  override def loadHook   (name: String): String = res.loadResource("patch/hooks/"+name)
+  override def loadLibrary(name: String): String = res.loadResource("patch/lib/"+name)
+  override def loadScreen (name: String): String = res.loadResource("patch/screen/"+name)
+  override def loadText   (name: String): String = res.loadResource("patch/text/"+name)
+}
 
-  private val luaPatchList = Map(
-    "contentswitch.lua"     -> MPPatchLuaOverride(injectAfter  = Seq("after_contentswitch.lua")),
-    "mainmenu.lua"          -> MPPatchLuaOverride(injectAfter  = Seq("after_mainmenu.lua")),
-    "modsmenu.lua"          -> MPPatchLuaOverride(injectAfter  = Seq("after_modsmenu.lua")),
-    "joiningroom.lua"       -> MPPatchLuaOverride(injectAfter  = Seq("after_joiningroom.lua")),
-    "stagingroom.lua"       -> MPPatchLuaOverride(injectBefore = Seq("before_stagingroom.lua"),
-                                                  injectAfter  = Seq("after_stagingroom.lua")),
-    "multiplayerselect.lua" -> MPPatchLuaOverride(injectBefore = Seq("before_multiplayerselect.lua")),
-    "lobby.lua"             -> MPPatchLuaOverride(injectBefore = Seq("intercept_bIsModding.lua")),
-    "mpgamesetupscreen.lua" -> MPPatchLuaOverride(injectBefore = Seq("intercept_bIsModding.lua")),
-    "mpgameoptions.lua"     -> MPPatchLuaOverride(injectAfter  = Seq("after_mpgameoptions.lua"))
-  )
+case class MPPatchLuaOverride(fileName: String, injectBefore: Seq[String] = Seq(), injectAfter: Seq[String] = Seq())
+case class MPPatchData(luaPatches: Seq[MPPatchLuaOverride], libraryFileNames: Seq[String],
+                       newScreenFileNames: Seq[String], textFileNames: Seq[String])
 
-  private val libraryFileNames = Seq("mppatch_runtime.lua", "mppatch_utils.lua", "mppatch_modutils.lua",
-                                     "mppatch_uiutils.lua", "mppatch_debug.lua")
-  private val libraryFiles = libraryFileNames.map(x =>
-    x -> res.loadResource(s"patch/lib/$x")
+class MPPatchLoader(data: MPPatchData, source: MPPatchFileSource) {
+  lazy val luaPatchList = data.luaPatches.map(x => x.fileName.toLowerCase -> x).toMap
+  lazy val libraryFiles = data.libraryFileNames.map(x =>
+    x -> source.loadLibrary(s"$x")
   ).toMap
-
-  private val newScreenFileNames = Seq("mppatch_multiplayerproxy")
-  private val newScreenFiles = newScreenFileNames.flatMap(x => Seq(
-    s"$x.lua" -> res.loadResource(s"patch/ui/$x.lua"),
-    s"$x.xml" -> res.loadResource(s"patch/ui/$x.xml")
+  val newScreenFiles = data.newScreenFileNames.flatMap(x => Seq(
+    s"$x.lua" -> source.loadScreen(s"$x.lua"),
+    s"$x.xml" -> source.loadScreen(s"$x.xml")
   )).toMap
+  val textFiles = data.textFileNames.map(x =>
+    x -> XML.loadString(source.loadText(x))
+  ).toMap
 
   private def loadWrapper(str: Seq[String]) =
     if(str.isEmpty) "" else {
@@ -69,42 +68,65 @@ object MPPatchDLC {
       "\n--- END INJECTED MPPATCH CODE ---"
     }
   private def getLuaFragment(path: String) = {
-    val code = res.loadResource("patch/hooks/"+path)
-    "-- source file: patch/hooks/"+path+"\n\n"+
+    val code = source.loadHook(path)
+    "-- source file: "+path+"\n\n"+
     code+(if(!code.endsWith("\n")) "\n" else "")
   }
-  private def getXmlFile(path: String) = XML.load(res.getResource(path))
-  private def findPatchTargets(path: Path, patches: Map[String, MPPatchLuaOverride]): Map[String, String] =
+  def patchFile(path: Path) = {
+    val fileName = path.getFileName.toString
+    luaPatchList.get(fileName.toLowerCase(Locale.ENGLISH)) match {
+      case Some(patch) =>
+        val runtime      = "include \"mppatch_runtime.lua\"\n"
+        val injectBefore = runtime +: patch.injectBefore.map(getLuaFragment)
+        val contents     = IOUtils.readFileAsString(path)
+        val injectAfter  = patch.injectAfter.map(getLuaFragment)
+        val finalFile    = loadWrapper(injectBefore)+"\n\n"+contents+"\n\n"+loadWrapper(injectAfter)
+        Some(finalFile)
+      case None =>
+        None
+    }
+  }
+}
+
+object MPPatchDLC {
+  val DLC_UUID          = UUID.fromString("df74f698-2343-11e6-89c4-8fef6d8f889e")
+  val DLC_VERSION       = 1
+  val DLC_UPDATEVERSION = 1
+
+  private val luaPatchList = Seq(
+    MPPatchLuaOverride("contentswitch.lua"     , injectAfter  = Seq("after_contentswitch.lua")),
+    MPPatchLuaOverride("mainmenu.lua"          , injectAfter  = Seq("after_mainmenu.lua")),
+    MPPatchLuaOverride("modsmenu.lua"          , injectAfter  = Seq("after_modsmenu.lua")),
+    MPPatchLuaOverride("joiningroom.lua"       , injectAfter  = Seq("after_joiningroom.lua")),
+    MPPatchLuaOverride("stagingroom.lua"       , injectBefore = Seq("before_stagingroom.lua"),
+                                                 injectAfter  = Seq("after_stagingroom.lua")),
+    MPPatchLuaOverride("multiplayerselect.lua" , injectBefore = Seq("before_multiplayerselect.lua")),
+    MPPatchLuaOverride("lobby.lua"             , injectBefore = Seq("intercept_bIsModding.lua")),
+    MPPatchLuaOverride("mpgamesetupscreen.lua" , injectBefore = Seq("intercept_bIsModding.lua")),
+    MPPatchLuaOverride("mpgameoptions.lua"     , injectAfter  = Seq("after_mpgameoptions.lua"))
+  )
+  private val libraryFileNames = Seq("mppatch_runtime.lua", "mppatch_utils.lua", "mppatch_modutils.lua",
+                                     "mppatch_uiutils.lua", "mppatch_debug.lua")
+  private val newScreenFileNames = Seq("mppatch_multiplayerproxy")
+  private val textFileNames = Seq("text_en_US.xml")
+  private val patchData = MPPatchData(luaPatchList, libraryFileNames, newScreenFileNames, textFileNames)
+
+  private def findPatchTargets(path: Path, loader: MPPatchLoader): Map[String, String] =
     IOUtils.listFiles(path).flatMap { file =>
-      val fileName = file.getFileName.toString
-      if(Files.isDirectory(file)) findPatchTargets(file, patches)
-      else patches.get(fileName.toLowerCase(Locale.ENGLISH)) match {
-        case Some(patch) =>
-          val runtime      = "include \"mppatch_runtime.lua\"\n"
-          val injectBefore = runtime +: patch.injectBefore.map(getLuaFragment)
-          val contents     = IOUtils.readFileAsString(file)
-          val injectAfter  = patch.injectAfter.map(getLuaFragment)
-          val finalFile    = loadWrapper(injectBefore)+"\n\n"+contents+"\n\n"+loadWrapper(injectAfter)
-          Seq(fileName -> finalFile)
-        case None =>
-          Seq()
-      }
+      if(Files.isDirectory(file)) findPatchTargets(file, loader)
+      else loader.patchFile(file).fold(Seq[(String, String)]())(x => Seq(file.getFileName.toString -> x))
     }.toMap
-
-  private val textFiles = Seq("text_en_US.xml").map(x =>
-    x -> getXmlFile("patch/xml/"+x)
-  ).toMap
-
   private def prepareList(map: Map[String, String]) = map.mapValues(_.getBytes(StandardCharsets.UTF_8))
-  private def findPathTargets(base: Path, platform: Platform, path: String*) =
-    findPatchTargets(platform.resolve(base, platform.assetsPath +: path: _*), luaPatchList)
+  private def findPathTargets(base: Path, loader: MPPatchLoader, platform: Platform, path: String*) =
+    findPatchTargets(platform.resolve(base, platform.assetsPath +: path: _*), loader)
   def generateBaseDLC(civBaseDirectory: Path, platform: Platform) = {
+    val loader = new MPPatchLoader(patchData, ResourceSource)
     DLCData(DLCManifest(DLC_UUID, DLC_VERSION, 1, "MPPatch", "MPPatch"),
-            DLCGameplay(textData = textFiles,
+            DLCGameplay(textData = loader.textFiles,
                         uiFiles = Map(
-                          "LuaPatches" -> prepareList(findPathTargets(civBaseDirectory, platform, "UI")),
-                          "Runtime"    -> prepareList(libraryFiles),
-                          "Screens"    -> prepareList(newScreenFiles)
+                          "LuaPatches" -> prepareList(findPathTargets(civBaseDirectory, loader, platform, "UI")),
+                          "Runtime"    -> prepareList(loader.libraryFiles),
+                          "Screens"    -> prepareList(loader.newScreenFiles)
                         ),
                         uiSkins = Seq(DLCUISkin("MPPatch", "BaseGame", "Common"))))
   }
