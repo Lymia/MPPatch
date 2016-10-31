12481bce0f4dbda9186f1e58e8c7a559fa394a5f
Lymia Aluysia <lymia@lymiahugs.com>
2016-10-31 08:41:56 -0500

    Try to fix stagingroom... again.

--- src/patch/ui/hooks/after_stagingroom.lua
+++ src/patch/ui/hooks/after_stagingroom.lua
@@ -1,40 +1,65 @@
 if _mpPatch and _mpPatch.enabled and _mpPatch.isModding then
-    local startLock = false
+    local confirmChat = "mppatch:gamelaunchcountdown:WgUUz7wWuxWFmjY02WFS0mka53nFDzJvD00zmuQHKyJT2wNHuWZvrDcejHv5rTyl"
+
+    local gameLaunchSet = false
+    local gameLaunchCountdown = 3
 
     local StartCountdownOld = StartCountdown
     function StartCountdown(...)
-        if not Matchmaking.IsHost() then
-            _mpPatch.overrideModsFromPreGame()
-        end
+        if gameLaunchSet then return end
         return StartCountdownOld(...)
     end
 
-    local function cancelOverride()
-        if not Matchmaking.IsHost() then
-            _mpPatch.debugPrint("Cancelling override.")
-            _mpPatch.patch.NetPatch.reset()
-        end
+    local StopCountdownOld = StopCountdown
+    function StopCountdown(...)
+        if gameLaunchSet then return end
+        return StopCountdownOld(...)
     end
 
-    local function OnPreGameDirtyHook(...)
-        if not ContextPtr:IsHidden() then
-            _mpPatch.debugPrint("cancelOverride from OnPreGameDirty")
-            cancelOverride()
-        end
-        return OnPreGameDirtyOld(...)
+    local HandleExitRequestOld = HandleExitRequest
+    function HandleExitRequest(...)
+        if gameLaunchSet then return end
+        return HandleExitRequestOld(...)
     end
-    Events.PreGameDirty.Add(OnPreGameDirtyHook)
+
+    local LaunchGameOld = LaunchGame
+    local function LaunchGameCountdown(timeDiff)
+        gameLaunchCountdown = gameLaunchCountdown - timeDiff
+        if gameLaunchCountdown <= 0 then
+            LaunchGameOld()
+            ContextPtr:ClearUpdate()
+        end
+    end
+    function LaunchGame(...)
+        if PreGame.IsHotSeatGame() then
+            return LaunchGameOld(...)
+        else
+            SendChat(confirmChat)
+            ContextPtr:ClearUpdate()
+            gameLaunchSet = true
+            ContextPtr:SetUpdate(LaunchGameCountdown)
+        end
+    end
+    Controls.LaunchButton:RegisterCallback(Mouse.eLClick, LaunchGame)
+
+    local OnChatOld = OnChat
+    function OnChat(...)
+        local fromPlayer, toPlayer, text = ...
+        if not Matchmaking.IsHost() and fromPlayer == m_HostID and text == confirmChat then
+            gameLaunchSet = true
+            _mpPatch.overrideModsFromPreGame()
+            return
+        end
+        return OnChatOld(...)
+    end
 
     local DequeuePopup = UIManager.DequeuePopup
     _mpPatch.patch.globals.rawset(UIManager, "DequeuePopup", function(this, ...)
         local context = ...
-        if context == ContextPtr then
-            _mpPatch.debugPrint("cancelOverride from DequeuePopup")
-            cancelOverride()
+        if context == ContextPtr and not Matchmaking.IsHost() then
+            _mpPatch.debugPrint("Cancelling override.")
+            _mpPatch.patch.NetPatch.reset()
         end
         return DequeuePopup(UIManager, ...)
     end)
-
-    -- Disable manual launch, helps with countdown related things.
-    Controls.LaunchButton:RegisterCallback(Mouse.eLClick, function() end)
 end
