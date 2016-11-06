ed917369f13f76521570ac5dfb60891bb8d8ab41
Lymia Aluysia <lymia@lymiahugs.com>
2016-11-06 10:49:41 -0600

    Attempt to fix joiningroom bug.

--- src/patch/ui/hooks/joiningroom_keepmods.lua
+++ src/patch/ui/hooks/joiningroom_keepmods.lua
@@ -1,4 +1,9 @@
 if _mpPatch and _mpPatch.canEnable then
+    local IsHotLoad = ContextPtr.IsHotLoad
+    _mpPatch.patch.globals.rawset(ContextPtr, "IsHotLoad", function(this, ...)
+        return IsHotLoad(this, ...) and not _mpPatch.isModding
+    end)
+
     Modding = _mpPatch.hookTable(Modding, {ActivateAllowedDLC = function(...)
         _mpPatch.overrideModsFromPreGame()
         return Modding._super.ActivateAllowedDLC(...)
