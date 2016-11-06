0ba9cdcd98e25cb9541d6df86e07c14a676c78e4
Lymia Aluysia <lymia@lymiahugs.com>
2016-11-06 06:40:35 -0600

    Fix contentswitch_debug.lua

--- src/patch/ui/hooks/contentswitch_debug.lua
+++ src/patch/ui/hooks/contentswitch_debug.lua
@@ -2,5 +2,5 @@ do
     local printFn = print
     local patch = DB.GetMemoryUsage("216f0090-85dd-4061-8371-3d8ba2099a70")
     if patch.__mppatch_marker then printFn = patch.debugPrint end
-    patchFn("Content switch.")
+    printFn("Content switch.")
 end
