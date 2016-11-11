e31cd6a9b7a28b45d2c536b585d8989558c9fd39
Lymia Aluysia <lymia@lymiahugs.com>
2016-11-10 20:24:43 -0600

    Fix unavailable leader, hopefully?

--- src/patch/ui/hooks/stagingroom_resetleaders.lua
+++ src/patch/ui/hooks/stagingroom_resetleaders.lua
@@ -1,7 +1,8 @@
 if _mpPatch and _mpPatch.loaded and _mpPatch.isModding then
     local function OnSystemUpdateUI(uiType, screen)
         if not ContextPtr:IsHidden() and uiType == SystemUpdateUIType.RestoreUI and screen == "StagingRoom" then
-            RefreshPlayerList()
+            _mpPatch.debugPrint("Executing OnPreGameDirty.")
+            OnPreGameDirty()
         end
     end
     Events.GameMessageChat.Add(OnSystemUpdateUI)
