090e34d6f2c5fce408f4448c0585737984d5c59c
Lymia Aluysia <lymia@lymiahugs.com>
2016-11-11 11:20:38 -0600

    Minor code style change

--- src/patch/ui/hooks/lobby_skip.lua
+++ src/patch/ui/hooks/lobby_skip.lua
@@ -1,10 +1,8 @@
-if _mpPatch and _mpPatch.loaded then
-    if ContextPtr:LookUpControl(".."):GetID() == "ModMultiplayerSelectScreen" then
-        ContextPtr:SetShowHideHandler(function(bIsHide, _)
-            if not bIsHide then
-                HostButtonClick()
-                UIManager:DequeuePopup(ContextPtr)
-            end
-        end)
-    end
+if _mpPatch and _mpPatch.loaded and ContextPtr:LookUpControl(".."):GetID() == "ModMultiplayerSelectScreen" then
+    ContextPtr:SetShowHideHandler(function(bIsHide, _)
+        if not bIsHide then
+            HostButtonClick()
+            UIManager:DequeuePopup(ContextPtr)
+        end
+    end)
 end
