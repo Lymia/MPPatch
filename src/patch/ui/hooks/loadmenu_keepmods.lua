718b9486c6a0ef679a7e265a7447fc4bf2ed88f0
Lymia Aluysia <lymia@lymiahugs.com>
2016-11-11 11:27:08 -0600

    Fix loadmenu

--- src/patch/ui/hooks/loadmenu_keepmods.lua
+++ src/patch/ui/hooks/loadmenu_keepmods.lua
@@ -1,8 +1,8 @@
 if _mpPatch and _mpPatch.loaded and ContextPtr:GetID() == "MPLoadGameScreen" then
     local override = {}
-    for _, func in ipairs({"HostServerGame", "HostInternetGame", "HostHotSeatGame", "HostLANGame"}) do
-        local func = Matchmaking[func]
-        override[func] = function(...)
+    for _, funcName in ipairs({"HostServerGame", "HostInternetGame", "HostHotSeatGame", "HostLANGame"}) do
+        local func = Matchmaking[funcName]
+        override[funcName] = function(...)
             local fileName = ...
             _mpPatch.overrideModsFromSaveFile(fileName)
             local ret = {func(...)}
