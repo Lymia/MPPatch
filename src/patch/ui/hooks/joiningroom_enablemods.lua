02469eeb52519a85c65d3a31a1f4772ae9129f31
Lymia Aluysia <lymia@lymiahugs.com>
2016-11-12 21:28:01 -0600

    Check for missing DLC in joiningroom.

--- src/patch/ui/hooks/joiningroom_enablemods.lua
+++ src/patch/ui/hooks/joiningroom_enablemods.lua
@@ -14,28 +14,46 @@ if _mpPatch and _mpPatch.loaded then
         Matchmaking.LeaveMultiplayerGame()
         UIManager:DequeuePopup(ContextPtr)
     end
+    local function checkTable(table, key)
+        if #table > 0 then
+            local messageTable = {Locale.Lookup(key)}
+            for _, name in ipairs(table) do
+                table.insert(messageTable, "[ICON_BULLET]"..name:gsub("%[", "("):gsub("%]", ")"))
+            end
+            joinFailed(table.concat(messageTable, "[NEWLINE]"))
+            return true
+        end
+        return false
+    end
     function OnConnectionCompete()
         if not Matchmaking.IsHost() then
             if _mpPatch.isModding then
-                local missingModList = {}
+                local modList = _mpPatch.decodeModsList()
+
+                local missingMods = {}
                 _mpPatch.debugPrint("Enabled mods for room:")
-                for _, mod in ipairs(_mpPatch.decodeModsList()) do
+                for _, mod in ipairs(modList) do
                     local missingText = ""
                     if not _mpPatch.isModInstalled(mod.ID, mod.Version) then
-                        table.insert(missingModList, mod.Name)
+                        table.insert(missingMods, mod.Name)
                         missingText = " (is missing)"
                     end
                     _mpPatch.debugPrint("- "..mod.Name..missingText)
                 end
-                -- TODO: Check for DLCs/mod compatibility
-                if #missingModList > 0 then
-                    local messageTable = {Locale.Lookup("TXT_KEY_MPPATCH_MOD_MISSING")}
-                    for _, name in ipairs(missingModList) do
-                        table.insert(messageTable, "[ICON_BULLET]"..name:gsub("%[", "("):gsub("%]", ")"))
+                if checkTable(missingMods, "TXT_KEY_MPPATCH_MOD_MISSING") then return end
+
+                local missingDlc = {}
+                local dlcDependencies = _mpPatch.getModDependencies(modList)
+                _mpPatch.debugPrint("DLC dependencies for room:")
+                for k, v in pairs(dlcDependencies) do
+                    local dlcName = Locale.Lookup("TXT_KEY_"..k.."_DESCRIPTION")
+                    if #v > 0 and not ContentManager.IsInstalled(k) then
+                        table.insert(missingDlc, dlcName)
                     end
-                    joinFailed(table.concat(messageTable, "[NEWLINE]"))
-                    return
+                    _mpPatch.debugPrint("- "..dlcName..", dependency count = "..#v..", "..
+                                        "is installed = "..tostring(ContentManager.IsInstalled(k)))
                 end
+                if checkTable(missingDlc, "TXT_KEY_MPPATCH_DLC_MISSING") then return end
             end
 
             enterLobby()
