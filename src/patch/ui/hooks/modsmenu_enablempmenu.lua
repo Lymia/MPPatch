f85e19b5e169dc6b6c1eb8fce73c6e66edc4c818
Lymia Aluysia <lymia@lymiahugs.com>
2016-11-12 20:40:02 -0600

    Add messages for lack of multiplayer support.

--- src/patch/ui/hooks/modsmenu_enablempmenu.lua
+++ src/patch/ui/hooks/modsmenu_enablempmenu.lua
@@ -6,7 +6,7 @@ if _mpPatch and _mpPatch.loaded then
     end)
     Controls.MultiPlayerButton:SetHide(false)
 
-    local function setIncompatible(control, name, title)
+    local function getModMessage(name, title)
         local mods = {}
         for _, v in ipairs(Modding.GetActivatedMods()) do
             if Modding.GetModProperty(v.ID, v.Version, name) ~= 1 then
@@ -14,14 +14,38 @@ if _mpPatch and _mpPatch.loaded then
             end
         end
         if #mods == 0 then
-            control:SetToolTipString(nil)
+            return nil
         else
-            control:SetToolTipString(Locale.Lookup(title).."[NEWLINE]"..table.concat(mods, "[NEWLINE]"))
+            return Locale.Lookup(title).."[NEWLINE]"..table.concat(mods, "[NEWLINE]")
         end
     end
+    local function setTooltip(control, name, title)
+        control:SetToolTipString(getModMessage(name, title))
+    end
+    local function showMessage(name, title)
+        local result = getModMessage(name, title)
+        if result then
+            Events.FrontEndPopup.CallImmediate(Locale.Lookup("TXT_KEY_MPPATCH_WARNING_PREFIX")..result)
+        end
+    end
+
+    local OnSinglePlayerClickOld = OnSinglePlayerClick
+    function OnSinglePlayerClick(...)
+        showMessage("SupportsSinglePlayer", "TXT_KEY_MPPATCH_NO_SINGLEPLAYER_SUPPORT")
+        return OnSinglePlayerClickOld(...)
+    end
+    Controls.SinglePlayerButton:RegisterCallback(Mouse.eLClick, OnSinglePlayerClick)
+
+    local OnMultiPlayerClickOld = OnMultiPlayerClick
+    function OnMultiPlayerClick(...)
+        showMessage("SupportsMultiplayer" , "TXT_KEY_MPPATCH_NO_MULTIPLAYER_SUPPORT" )
+        return OnMultiPlayerClickOld(...)
+    end
+    Controls.MultiPlayerButton:RegisterCallback(Mouse.eLClick, OnMultiPlayerClick)
+
     local function onShowHideHook()
-        setIncompatible(Controls.SinglePlayerButton, "SupportsSinglePlayer", "TXT_KEY_MPPATCH_NO_SINGLEPLAYER_SUPPORT")
-        setIncompatible(Controls.MultiPlayerButton , "SupportsMultiplayer" , "TXT_KEY_MPPATCH_NO_MULTIPLAYER_SUPPORT" )
+        setTooltip(Controls.SinglePlayerButton, "SupportsSinglePlayer", "TXT_KEY_MPPATCH_NO_SINGLEPLAYER_SUPPORT")
+        setTooltip(Controls.MultiPlayerButton , "SupportsMultiplayer" , "TXT_KEY_MPPATCH_NO_MULTIPLAYER_SUPPORT" )
     end
     Modding = _mpPatch.hookTable(Modding, {
         AllEnabledModsContainPropertyValue = function(...)
