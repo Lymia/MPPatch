3fc3bb834ec224ad4264328622195a27a4faba0d
Lymia Aluysia <lymia@lymiahugs.com>
2016-11-12 20:31:52 -0600

    Always enable both singleplayer and multiplayer modes on main mods menu.

--- src/patch/ui/hooks/modsmenu_enablempmenu.lua
+++ src/patch/ui/hooks/modsmenu_enablempmenu.lua
@@ -5,4 +5,34 @@ if _mpPatch and _mpPatch.loaded then
         UIManager:QueuePopup(Controls.ModMultiplayerSelectScreen, PopupPriority.ModMultiplayerSelectScreen)
     end)
     Controls.MultiPlayerButton:SetHide(false)
+
+    local function setIncompatible(control, name, title)
+        local mods = {}
+        for _, v in ipairs(Modding.GetActivatedMods()) do
+            if Modding.GetModProperty(v.ID, v.Version, name) ~= 1 then
+                table.insert(mods, "[BULLET]".._mpPatch.getModName(v.ID, v.Version))
+            end
+        end
+        if #mods == 0 then
+            control:SetToolTipString(nil)
+        else
+            control:SetToolTipString(Locale.Lookup(title).."[NEWLINE]"..table.concat(mods, "[NEWLINE]"))
+        end
+    end
+    local function onShowHideHook()
+        setIncompatible(Controls.SinglePlayerButton, "SupportsSinglePlayer", "TXT_KEY_MPPATCH_NO_SINGLEPLAYER_SUPPORT")
+        setIncompatible(Controls.MultiPlayerButton , "SupportsMultiplayer" , "TXT_KEY_MPPATCH_NO_MULTIPLAYER_SUPPORT" )
+    end
+    Modding = _mpPatch.hookTable(Modding, {
+        AllEnabledModsContainPropertyValue = function(...)
+            local name = ...
+            if name == "SupportsSinglePlayer" then
+                onShowHideHook()
+                return true
+            elseif name == "SupportsMultiplayer" then
+                return true
+            end
+            return Modding._super.AllEnabledModsContainPropertyValue(...)
+        end
+    })
 end
