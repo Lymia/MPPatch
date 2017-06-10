ddd0669aa982093737f31373e0e75b2026a51114
Lymia Aluysia <lymia@lymiahugs.com>
2017-06-10 06:24:24 -0500

    Move backup clientIsPatched command.

--- src/patch/ui/hooks/frontend/staging/stagingroom_resetleaders.lua
+++ src/patch/ui/hooks/frontend/staging/stagingroom_resetleaders.lua
@@ -25,6 +25,7 @@ if _mpPatch_activateFrontEnd then
         if not ContextPtr:IsHidden() and not Matchmaking.IsHost() and
            uiType == SystemUpdateUIType.RestoreUI and screen == "StagingRoom" then
             _mpPatch.debugPrint("Requesting user info update.")
+            _mpPatch.net.clientIsPatched(_mpPatch.protocolVersion)
             _mpPatch.net.sendPlayerData()
         end
     end)
