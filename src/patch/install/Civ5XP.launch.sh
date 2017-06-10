1d902b7d53cf7fe33225c5b5cc9a593b37721958
Lymia Aluysia <lymia@lymiahugs.com>
2017-06-10 03:56:23 -0500

    Add --no-steam flag to Civ5XP.launch

--- src/patch/install/Civ5XP.launch.sh
+++ src/patch/install/Civ5XP.launch.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 path="$(dirname "$0")"
 export LD_LIBRARY_PATH="$path:$LD_LIBRARY_PATH"
-export SteamAppId=8930
+if [ "$1" -ne "--no-steam" ]; then
+    export SteamAppId=8930
+fi
 export LD_PRELOAD="mppatch_core.so"
 exec -a Civ5XP "$path/Civ5XP.orig" "$@"
