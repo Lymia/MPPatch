caf2e2de3505b01fce36b480e2460ff151347b73
Lymia Aluysia <lymia@lymiahugs.com>
2017-06-20 23:03:16 -0500

    Fix mac launcher script.

--- src/patch/install/Civilization Vsub.sh
+++ src/patch/install/Civilization Vsub.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 path="$(dirname "$0")"
-export DYLD_INSERT_LIBRARIES="$path/mppatch_core.so"
+export DYLD_INSERT_LIBRARIES="$path/mppatch_core.dylib"
 exec -a "Civilization Vsub" "$path/Civilization Vsub orig" "$@"
