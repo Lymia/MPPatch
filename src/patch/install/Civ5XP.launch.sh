7f8f4809df79377b40653e362d51d06ed3b26cba
Lymia Aluysia <lymia@lymiahugs.com>
2019-01-02 06:32:06 -0600

    Don't override preexisting LD_PRELOADs in the Linux launch scripts.

--- src/patch/install/Civ5XP.launch.sh
+++ src/patch/install/Civ5XP.launch.sh
@@ -4,6 +4,6 @@ path="$(dirname "$0")"
 export LD_LIBRARY_PATH="$path:$LD_LIBRARY_PATH"
 export SteamAppId=8930
 
-export LD_PRELOAD="mppatch_core.so"
+export LD_PRELOAD="mppatch_core.so $LD_PRELOAD"
 
 exec -a Civ5XP "$path/Civ5XP.orig" "$@"
--- src/patch/install/Civ5XP.sh
+++ src/patch/install/Civ5XP.sh
@@ -8,5 +8,5 @@ fi
 
 path="$(dirname "$0")"
 export LD_LIBRARY_PATH="$path:$LD_LIBRARY_PATH"
-export LD_PRELOAD="mppatch_core.so"
+export LD_PRELOAD="mppatch_core.so $LD_PRELOAD"
 exec -a Civ5XP "$path/Civ5XP.orig" "$@"
