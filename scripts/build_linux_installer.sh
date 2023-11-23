f50f08c9b06be22f19e2d9312612d190d63d66ca
Alissa Rao <lymia@lymia.moe>
2023-11-23 05:14:24 -0800

    Do not assume target exists in build_linux_installer.sh

--- scripts/build_linux_installer.sh
+++ scripts/build_linux_installer.sh
@@ -24,7 +24,7 @@
 
 echo "Extracting native tarballs..."
 rm -rfv target/native-bin || exit 1
-mkdir -v target/native-bin || exit 1
+mkdir -vp target/native-bin || exit 1
 cd target/native-bin || exit 1
   if [ -e ../../mppatch_win32_natives.tar.gz ]; then
     tar -xv -f ../../mppatch_linux_natives.tar.gz
