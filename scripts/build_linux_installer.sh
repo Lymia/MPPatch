53359ac8b17f0838a1153a8984b1ba5f8e563538
Alissa Rao <lymia@lymia.moe>
2023-11-23 05:35:45 -0800

    Protect against garbage returned from sbt.

--- scripts/build_linux_installer.sh
+++ scripts/build_linux_installer.sh
@@ -43,7 +43,8 @@ sbt nativeImage || exit 1
 chmod +x target/native-image/*.so || exit 1
 
 echo "Building assembly jar..."
-cp "$(sbt "print assembly" --error || exit 1)" target/native-image/assembly.jar || exit 1
+ASSEMBLY_JAR="$(sbt "print assembly" --error || exit 1)"
+cp "$(echo "$ASSEMBLY_JAR" | head -n 1)" target/native-image/assembly.jar || exit 1
 
 echo "Creating Linux installer tarball..."
 cd target/native-image || exit 1
